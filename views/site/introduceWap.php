61ff093b0df2a10731a9b193679043f2be9a3560
maxwelldu <dcj3sjt@163.com>
2016-01-06 18:41:34 +0800

    opt index wap page

--- views/site/introduceWap.php
+++ views/site/introduceWap.php
@@ -10,53 +10,21 @@
     <link rel="shortcut icon" href="/favicon.ico"/>
     <script src="http://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
     <style>
-        html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font:inherit;font-size:100%;vertical-align:baseline}html{line-height:1}ol,ul{list-style:none}table{border-collapse:collapse;border-spacing:0}caption,th,td{text-align:left;font-weight:normal;vertical-align:middle}q,blockquote{quotes:none}q:before,q:after,blockquote:before,blockquote:after{content:"";content:none}a img{border:none}article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section,summary{display:block}.wap-icons-sprite,.wap-icons-btn_android,.wap-icons-btn_android_p,.wap-icons-btn_ios,.wap-icons-btn_ios_p{background-repeat:no-repeat}.wap-icons-btn_android{background-position:0 0}.wap-icons-btn_android_p{background-position:0 -96px}.wap-icons-btn_ios{background-position:0 -192px}.wap-icons-btn_ios_p{background-position:0 -288px}html,body{position:relative;height:100%}body{background:#FFF;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:14px;color:#000;margin:0;padding:0}.swiper-container{width:100%;height:100%}.swiper-slide{text-align:center;background:#fff}#js_title_container{position:relative;width:100%;background-color:#FFFFFF}#js_title_container img{position:absolute;width:100%;left:0;bottom:0}#js_download_container{background-color:#FFFFFF}#js_download_container .js_download_link_container{width:49%;display:inline-block;text-align:center}#js_download_container #js_download_link_ios{display:block;height:48px;margin:8px 4px 8px 8px;background-color:#31CCAA;text-align:center}#js_download_container #js_download_link_ios:active{background-color:#2bb395}#js_download_container #js_download_link_android{display:block;height:48px;margin:8px 8px 8px 4px;background-color:#31CCAA;text-align:center}#js_download_container #js_download_link_android:active{background-color:#2bb395}.js_dl{display:inline-block;height:48px;line-height:48px;vertical-align:middle;padding-left:30px}.js_dl_bg_ios{background-size:26px 26px}.js_dl_bg_android{background:url("http://image.izudian.com/web_image/20150819/ic_android.png") no-repeat 0 10px;background-size:26px 26px}.js_dl_text{color:#FFFFFF;font-size:14px;font-weight:200}.js_scene{position:relative}.js_scene img{position:absolute;width:100%;left:0;bottom:0}#js_scene_1{background-color:#FFFFFF}#js_scene_2{background-color:#FFC565}#js_scene_3{background-color:#6192D8}#js_scene_4{background-color:#FFFFFF}#js_scene_4 img{position:absolute;width:100%;left:0;top:0}.ui-body-c,.ui-overlay-c{background:none}#js_footer{position:absolute;left:0;bottom:0;text-align:center;width:100%;padding:10px}#js_footer .js_footer_info{color:#333333;font-size:8px;line-height:10px;margin-bottom:4px}#js_footer .js_footer_link{color:#333333;font-size:8px;text-decoration:none}
     </style>
 </head>
 <body>
-<div class="swiper-container">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div id="js_scene_title" class="swiper-slide">
-                <img src="/images/index.jpg" width="100%"/>
-        </div>
-        <div id="js_scene_1" class="swiper-slide js_scene">
-            <div id="js_footer">
+<div >
+    <img src="/images/index_wap.jpg" width="100%"/>
+    <div id="footer">
 
-                <p class="js_footer_info">©2016 Beijing Someet Technology Co., Ltd. All rights reserved. </p>
+        <p >©2016 Beijing Someet Technology Co., Ltd. All rights reserved. </p>
 
-                <p class="js_footer_info">备案号：京ICP备15052155号 &nbsp; &nbsp;&nbsp;
-                    <a class="js_footer_link" href="mailto:hr@someet.so">联系我们</a>
-                </p>
-            </div>
-        </div>
+        <p >备案号：京ICP备15052155号 &nbsp; &nbsp;&nbsp;
+            <a href="mailto:hr@someet.so">联系我们</a>
+        </p>
     </div>
 </div>
 </body>
-<script>
-
-    $(document).ready(function () {
-        var screenHeight = jQuery(window).height();
-        var titleHeight = screenHeight - 64;
-        $('#js_title_container').css('height', titleHeight);
-
-        $(document).ready(function () {
-            //initialize swiper when document ready
-            var mySwiper = new Swiper('.swiper-container', {
-                // Optional parameters
-                direction: 'vertical',
-                loop: false
-            })
-        });
-    });
-
-    $(window).resize(function () {
-        var screenHeight = jQuery(window).height();
-        var titleHeight = screenHeight - 64;
-        $('#js_title_container').css('height', titleHeight);
-    });
-</script>
 <script>
     var _hmt = _hmt || [];
     (function() {
